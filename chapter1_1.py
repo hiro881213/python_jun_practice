5ddd83a986a44326aee13e856b3c59c777194723
hiro881213 <nikep.godbard@gmail.com>
2025-04-28 00:05:35 +0900

    変数に違う型の値を入れる

--- chapter1_1.py
+++ chapter1_1.py
@@ -6,3 +6,18 @@ print(num)
 # 文字列の宣言
 name = 'Mike'
 print(name)
+
+# type関数
+print(type(num))
+print(type(name))
+
+# Boolean型
+is_ok = True
+print(is_ok)
+print(type(is_ok))
+
+#変数に違う型の値を入れる
+num2 = 2
+name2 = 'Mike'
+num2 = name2
+print(num2, type(num2))
