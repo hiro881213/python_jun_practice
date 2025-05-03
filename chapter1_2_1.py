3323d2c55ca6407993116dd58e824edd5dc94758
hiro881213 <nikep.godbard@gmail.com>
2025-05-03 15:18:05 +0900

    Add new chapter

--- chapter1_2_1.py
+++ chapter1_2_1.py
@@ -1,5 +1,6 @@
 # 1.2. 文字列のさまざまな操作方法
 # 1.2.1. 文字列の基本的な使い方
+# 1.2.1.1. 文字列を表示する
 # 文字列を表示する
 s = 'hello'
 print(s)
@@ -34,3 +35,28 @@ line2
 line3\
 """)
 print("################")
+
+# 1.2.1.2. 文字列を連結する
+# 文字列を繰り返し出力する
+print('Hi. ' * 3)
+
+# 文字列を連結する
+print('Hi. ' * 3 + 'Mike. ')
+
+# リテラル同士を連結する
+print('Py' + 'thon')
+print('Py''thon')
+
+# 変数とリテラルを連結する
+prefix = 'Py'
+print(prefix + 'thon')
+
+# 複数行にわたる文字列の連結
+print('aaaaaaaaaaaaaaaaaaaaa'
+      'bbbbbbbbbbbbbbbbbbbbb')
+
+# 複数行にわたる文字列の連結2
+s = 'aaaaaaaaaaaaaaaaaaaaa'\
+      'bbbbbbbbbbbbbbbbbbbbb'
+
+print(s)
--- chapter1_2_2.py
+++ chapter1_2_2.py
@@ -1,25 +1,3 @@
 # 1.2. 文字列のさまざまな操作方法
-# 1.2.2. 文字列を連結する
-# 文字列を繰り返し出力する
-print('Hi. ' * 3)
+# 1.2.2. 文字列のインデックスとスライスを使う
 
-# 文字列を連結する
-print('Hi. ' * 3 + 'Mike. ')
-
-# リテラル同士を連結する
-print('Py' + 'thon')
-print('Py''thon')
-
-# 変数とリテラルを連結する
-prefix = 'Py'
-print(prefix + 'thon')
-
-# 複数行にわたる文字列の連結
-print('aaaaaaaaaaaaaaaaaaaaa'
-      'bbbbbbbbbbbbbbbbbbbbb')
-
-# 複数行にわたる文字列の連結2
-s = 'aaaaaaaaaaaaaaaaaaaaa'\
-      'bbbbbbbbbbbbbbbbbbbbb'
-
-print(s)
