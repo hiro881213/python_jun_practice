dd31512cb5b12e261489e5ea6cdf7273d69066fa
hiro881213 <nikep.godbard@gmail.com>
2025-05-04 21:46:38 +0900

    findメソッド,rfindメソッド

--- chapter1_2_3.py
+++ chapter1_2_3.py
@@ -1,9 +1,16 @@
 # 1.2. 文字列のさまざまな操作方法
 # 1.2.3. 文字列のメソッドを利用する
+s = 'My name is Mike. Hi Mike.'
+
 # startsWithメソッド
-startsWithVal = 'My name is Mike. Hi Mike.'
-is_start1 = startsWithVal.startswith('My')
-is_start2 = startsWithVal.startswith('X')
+is_start1 = s.startswith('My')
+is_start2 = s.startswith('X')
 
 print(is_start1)
 print(is_start2)
+
+# findメソッド
+print(s.find('Mike'))
+
+# rfindメソッド
+print(s.rfind('Mike'))
