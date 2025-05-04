874de3c18c98f0832cf710f004ad416e96d96f6d
hiro881213 <nikep.godbard@gmail.com>
2025-05-05 00:51:12 +0900

    f-stringによる複数の文字列の挿入

--- chapter1_2_4.py
+++ chapter1_2_4.py
@@ -17,6 +17,11 @@ print("My name is {0} {1}. Watashi wa {1} {0}.".format("Keanu", "Reeves"))
 # インデックスを文字列にする
 print("My name is {name} {family}. Watashi wa {family} {name}.".format(name = "Keanu", family = "Reeves"))
 
-# f-string1
+# f-string
 a = "a"
 print(f"a is {a}.")
+
+# f-stringによる複数の文字列の挿入
+x, y, z = 1, 2, 3
+print(f"a is {x} {y} {z}.")
+print(f"a is {z} {y} {x}.")
