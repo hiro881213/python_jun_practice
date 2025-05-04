1c401dfba1e34883d2edbc42eb6e3df51f752da3
hiro881213 <nikep.godbard@gmail.com>
2025-05-04 21:39:47 +0900

    startsWithメソッド2

--- chapter1_2_3.py
+++ chapter1_2_3.py
@@ -2,6 +2,8 @@
 # 1.2.3. 文字列のメソッドを利用する
 # startsWithメソッド
 startsWithVal = 'My name is Mike. Hi Mike.'
-is_start = startsWithVal.startswith('My')
+is_start1 = startsWithVal.startswith('My')
+is_start2 = startsWithVal.startswith('X')
 
-print(is_start)
+print(is_start1)
+print(is_start2)
