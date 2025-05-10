02be80f3e9f2654f833f7b19e89af3daa1d4d838
hiro881213 <nikep.godbard@gmail.com>
2025-05-10 20:58:59 +0900

    参照渡し

--- chapter2.1.4.2.py
+++ chapter2.1.4.2.py
@@ -1,7 +1,7 @@
 # 2.1. 複数データを並列にまとめる
 # 2.1.4. リストのコピー
 # 2.1.4.2. 値渡しと参照渡し
-# ID関数1
+# 値渡し
 X = 20
 Y = X
 Y = 5
@@ -10,3 +10,12 @@ print(id(X))
 print(id(Y))
 print(X)
 print(Y)
+
+# 参照渡し
+A = ['a', 'b']
+B= A
+
+print(id(A))
+print(id(B))
+print(A)
+print(B)
