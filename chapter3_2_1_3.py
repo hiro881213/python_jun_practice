1082d328ba0b4f2f074e552779f0bd4d890e7a8d
hiro881213 <nikep.godbard@gmail.com>
2025-05-15 23:38:27 +0900

    複数のelif文

--- chapter3_2_1_3.py
+++ chapter3_2_1_3.py
@@ -8,3 +8,15 @@ elif x == 0:
     print('zero')
 else:
     print('positive')
+
+# 複数のelif文
+x = 10
+
+if x < 0:
+    print('negative')
+elif x == 0:
+    print('zero')
+elif x == 10:
+    print('10')
+else:
+    print('positive')
