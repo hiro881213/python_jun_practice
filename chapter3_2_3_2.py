67bc444cd622d0a306cca6a93d02658d47f88650
hiro881213 <nikep.godbard@gmail.com>
2025-05-16 21:27:10 +0900

    Boolean型の変数の否定

--- chapter3_2_3_2.py
+++ chapter3_2_3_2.py
@@ -1,8 +1,12 @@
 # 3.2. 条件分岐
 # 3.2.3. inとnotの利用
 # 3.2.3.2. notの使い所
-# Boolean型をそのまま書く
+# Boolean型の変数をそのまま書く
 is_ok = True
 
 if is_ok:
+    print('hello')
+
+# Boolean型の変数の否定
+if not is_ok:
     print('hello')
