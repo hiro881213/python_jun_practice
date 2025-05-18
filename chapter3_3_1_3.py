026f19204cf9a2aa31ecadc3b08d49aa96ae8a39
hiro881213 <nikep.godbard@gmail.com>
2025-05-18 19:27:02 +0900

    continue文

--- chapter3_3_1_3.py
+++ chapter3_3_1_3.py
@@ -1,3 +1,15 @@
 # 3.3. 繰り返し処理
 # 3.3.1. while文,continue文,break文の使い方
 # 3.3.1.2. continue文
+count = 0
+
+while True:
+    if count >= 5:
+        break
+
+    if count == 2:
+        count += 1
+        continue
+
+    print(count)
+    count += 1
