e35fcb1bb15d66c289e0da37a2213546e7414c48
hiro881213 <nikep.godbard@gmail.com>
2025-05-19 00:14:10 +0900

    3ずつ増えるように取り出す

--- chapter3_3_6.py
+++ chapter3_3_6.py
@@ -4,13 +4,21 @@
 # range関数を使わないパターン
 num_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+print('###############')
 for i in num_list:
     print(i)
 
 # range関数を利用するパターン
+print('###############')
 for i in range(10):
     print(i)
 
 # range関数で始まりと終わりを指定する
+print('###############')
 for i in range(2, 10):
+    print(i)
+
+# 3ずつ増えるように取り出す
+print('###############')
+for i in range(2, 10, 3):
     print(i)
