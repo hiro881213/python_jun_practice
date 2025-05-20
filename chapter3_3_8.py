9e24ff69bc1f7b5c565a78d058a56d6eec10adf2
hiro881213 <nikep.godbard@gmail.com>
2025-05-20 23:57:46 +0900

    zip関数

--- chapter3_3_8.py
+++ chapter3_3_8.py
@@ -1,9 +1,13 @@
 # 3.3. 繰り返し処理
 # 3.3.8. zip関数
-# zip関数を使わない
 days = ['Mon', 'Tue', 'Wed']
 fruits = ['apple', 'banana', 'orange']
 drinks = ['coffee','tea', 'beer']
 
+# zip関数を使わない
 for i in range(len(days)):
     print(days[i], fruits[i], drinks[i])
+
+# zip関数
+for day,  fruit, drink in zip(days, fruits, drinks):
+    print(day, fruit, drink)
