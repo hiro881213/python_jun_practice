3917b06671b446eb72e44d1703de88eb7fc2c1df
hiro881213 <nikep.godbard@gmail.com>
2025-05-23 21:02:35 +0900

    引数２

--- chapter4_1_1_2.py
+++ chapter4_1_1_2.py
@@ -5,3 +5,15 @@ def what_is_this(color):
     print(color)
 
 what_is_this('red')
+
+# 引数２
+def what_is_this2(color):
+    if color == 'red':
+        return 'tomato'
+    elif color == 'green':
+        return 'green pepper'
+    else:
+        return "I don't know"
+
+result = what_is_this2('red')
+print(result)
