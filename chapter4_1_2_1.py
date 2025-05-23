00c976cf6223bec4b03fe6c2b117f5a1449a2339
hiro881213 <nikep.godbard@gmail.com>
2025-05-24 04:51:41 +0900

    位置引数

--- chapter4_1_2_1.py
+++ chapter4_1_2_1.py
@@ -1,3 +1,9 @@
 # 4.1. 関数
 # 4.1.2.　位置引数、キーワード引数、デフォルト引数
 # 4.1.2.1.　位置引数
+def menu(entree, drink, dessert):
+    print('entree=', entree)
+    print('drink=', drink)
+    print('dessert=', dessert)
+
+menu('beef', 'beer', 'ice')
