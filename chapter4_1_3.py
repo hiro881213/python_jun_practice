62d5e5f3ba3044f5b0c0bafb41518902175c06a4
hiro881213 <nikep.godbard@gmail.com>
2025-05-24 16:48:49 +0900

    デフォルト引数に空のリストを使う

--- chapter4_1_3.py
+++ chapter4_1_3.py
@@ -1,2 +1,15 @@
 # 4.1. 関数
 # 4.1.3. デフォルト引数でリストや辞書型を使う
+def sample_func(x, l = []):
+    l.append(x)
+
+    return l
+
+# デフォルト引数に空のリストを使う
+y = [1, 2, 3]
+r = sample_func(100, y)
+print(r)
+
+y = [1, 2, 3]
+r = sample_func(200, y)
+print(r)
