8b7388f93c1f8b1e2cedc09d3eee5331c94926f3
hiro881213 <nikep.godbard@gmail.com>
2025-05-24 18:47:20 +0900

    キーワード引数を辞書にして渡す

--- chapter4_1_5_2.py
+++ chapter4_1_5_2.py
@@ -1,3 +1,14 @@
 # 4.1. 関数
 # 4.1.5. キーワード引数を辞書化してまとめる
 # キーワード引数を辞書にして渡す
+def menu(**kwargs):
+    for k, v in kwargs.items():
+        print(k, v)
+
+d = {
+    'entree': 'beef',
+    'drink': 'ice coffee',
+    'dessert': 'ice'
+}
+
+menu(**d)
