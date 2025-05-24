20307b43b9313bd3408d1f2766040b88da39783b
hiro881213 <nikep.godbard@gmail.com>
2025-05-24 18:14:14 +0900

    引数を渡す際にアンパッキングする

--- chapter4_1_4_2.py
+++ chapter4_1_4_2.py
@@ -1,3 +1,11 @@
 # 4.1. 関数
 # 4.1.4. 位置引数をタプル化してまとめる
 # 引数を渡す際にアンパッキングする
+def say_something(word, *args):
+    print('word = ', word)
+
+    for arg in args:
+        print(arg)
+
+t = ('Mike', 'Nancy')
+say_something('Hi!', *t)
