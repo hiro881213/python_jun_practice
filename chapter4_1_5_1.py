b213151b50cdbcd0547228e29ef4d950bf9b0be4
hiro881213 <nikep.godbard@gmail.com>
2025-05-24 18:25:54 +0900

    キーワード引数を辞書型で受け取る

--- chapter4_1_5_1.py
+++ chapter4_1_5_1.py
@@ -5,3 +5,9 @@ def menu(entree = 'beef', drink = 'wine'):
     print(entree, drink)
 
 menu(entree = 'beef', drink = 'coffee')
+
+# キーワード引数を辞書型で受け取る
+def menu2(**kwargs):
+    print(kwargs)
+
+menu2(entree = 'beef', drink = 'coffee')
