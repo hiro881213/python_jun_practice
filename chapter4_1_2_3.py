dbcea0d60e66d03d096a4214f85ff8084f556655
hiro881213 <nikep.godbard@gmail.com>
2025-05-24 16:39:31 +0900

    デフォルト引数

--- chapter4_1_2_3.py
+++ chapter4_1_2_3.py
@@ -1,3 +1,9 @@
 # 4.1. 関数
 # 4.1.2.　位置引数、キーワード引数、デフォルト引数
 # 4.1.2.3. デフォルト引数
+def menu(entree = 'beef', drink = 'wine', dessert = 'ice'):
+    print('entree = ', entree)
+    print('drink = ', drink)
+    print('dessert = ', dessert)
+
+menu()
