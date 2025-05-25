93a568d6b16cdc8cdb917491204351e9f9a70117
hiro881213 <nikep.godbard@gmail.com>
2025-05-25 15:39:40 +0900

    複数のデコレータをラップする

--- chapter4_2_2_3.py
+++ chapter4_2_2_3.py
@@ -1,2 +1,21 @@
 # 4.2. 関数の応用
 # 4.2.2. デコレータで関数の前後に処理を加える
+# 複数のデコレータをラップする
+def print_more(func):
+    def wrapper(*args, **kwargs):
+        print('func:', func.__name__)
+        print('args:', args)
+        print('kwargs:', kwargs)
+        result = func(*args, **kwargs)
+        print('result:', result)
+
+        return result
+
+    return wrapper
+
+@print_more
+def add_num(a, b):
+    return a + b
+
+r = add_num(10, 20)
+print(r)
