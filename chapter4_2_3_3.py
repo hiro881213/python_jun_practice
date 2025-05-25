7f2bd4455c2c3e106b33df7c15203534c15812a3
hiro881213 <nikep.godbard@gmail.com>
2025-05-26 02:45:16 +0900

    lambda関数を利用しないパターン

--- chapter4_2_3_3.py
+++ chapter4_2_3_3.py
@@ -1,2 +1,21 @@
 # 4.2. 関数の応用
 # 4.2.3. lambdaの利用
+# 曜日のリスト
+l = ['Mon', 'tue', 'Wed', 'Thu', 'Fri', 'sat', 'Sun']
+
+# 文字列に関数を適用する関数
+def change_words(words, func):
+    for word in words:
+        print(func(word))
+
+def sample_func(word):
+    return word.capitalize()
+
+def sample_func2(word):
+    return word.lower()
+
+change_words(l, sample_func)
+print('---------------------------')
+change_words(l, sample_func2)
+print('---------------------------')
+
