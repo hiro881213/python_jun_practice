dfd1254f24e8be92b7cb7320ca67cc4ff474bd62
hiro881213 <nikep.godbard@gmail.com>
2025-05-25 13:24:15 +0900

    関数の説明を記述

--- chapter4_1_6.py
+++ chapter4_1_6.py
@@ -1,2 +1,16 @@
 # 4.1. 関数
 # 4.1.6. docstring
+# 関数の説明を記述
+def example_func(param1, param2):
+    """
+    Args:
+        param1(int): The First parameter
+        param2(str): The Second parameter
+
+    Returns:
+        bool: The return value. True for success, False otherwise
+    """
+    print(param1)
+    print(param2)
+
+    return True
