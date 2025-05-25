7f7814d73ff87cbcf6c391c9212d5fcd147bbc53
hiro881213 <nikep.godbard@gmail.com>
2025-05-26 02:23:27 +0900

    文字列の語頭を大文字にする関数を作成する

--- chapter4_2_3_1.py
+++ chapter4_2_3_1.py
@@ -7,3 +7,7 @@ l = ['Mon', 'tue', 'Wed', 'Thu', 'Fri', 'sat', 'Sun']
 def change_words(words, func):
     for word in words:
         print(func(word))
+
+# 文字列の語頭を大文字にする関数
+def sample_func(word):
+    return word.capitalize()
