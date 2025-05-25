b026a8dba78a33ee88c6dcb165b74d4ab95108ca
hiro881213 <nikep.godbard@gmail.com>
2025-05-26 02:19:32 +0900

    文字列に関数を適用する関数を作成する

--- chapter4_2_3_1.py
+++ chapter4_2_3_1.py
@@ -2,3 +2,8 @@
 # 4.2.3. lambdaの利用
 # 曜日のリスト
 l = ['Mon', 'tue', 'Wed', 'Thu', 'Fri', 'sat', 'Sun']
+
+# 文字列に関数を適用する関数
+def change_words(words, func):
+    for word in words:
+        print(func(word))
