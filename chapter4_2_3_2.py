33acd3b1cc652d90c55b56497fb0fb8f3e756e81
hiro881213 <nikep.godbard@gmail.com>
2025-05-26 02:30:49 +0900

    ラムダ関数で記載する

--- chapter4_2_3_2.py
+++ chapter4_2_3_2.py
@@ -1,2 +1,13 @@
 # 4.2. 関数の応用
 # 4.2.3. lambdaの利用
+# 曜日のリスト
+l = ['Mon', 'tue', 'Wed', 'Thu', 'Fri', 'sat', 'Sun']
+
+# 文字列に関数を適用する関数
+def change_words(words, func):
+    for word in words:
+        print(func(word))
+
+sample_func = lambda word: word.capitalize()
+
+change_words(l, sample_func)
