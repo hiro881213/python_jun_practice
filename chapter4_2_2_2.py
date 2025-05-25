512f6a51f2a82338191e7a926c25fe1c8f78e7ed
hiro881213 <nikep.godbard@gmail.com>
2025-05-25 15:31:42 +0900

    デコレータの省略

--- chapter4_2_2_2.py
+++ chapter4_2_2_2.py
@@ -1,2 +1,18 @@
 # 4.2. 関数の応用
 # 4.2.2. デコレータで関数の前後に処理を加える
+# デコレータの省略
+def print_info(func):
+    def wrapper(*args, **kwargs):
+        print('start')
+        result = func(*args, **kwargs)
+        print('end')
+
+        return result
+    return wrapper
+
+@print_info
+def add_num(a, b):
+    return a + b
+
+r = add_num(10, 20)
+print(r)
