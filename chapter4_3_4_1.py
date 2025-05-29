a10cc449762090f7c9f4a6a680fbf009648b8a4b
hiro881213 <nikep.godbard@gmail.com>
2025-05-30 01:14:50 +0900

    forループでジェネレータを作成する

--- chapter4_3_4_1.py
+++ chapter4_3_4_1.py
@@ -1,2 +1,18 @@
 # 4.3. 内包表記でリストを生成する
 # 4.3.4. ジェネレータ内包表記の書き方
+# forループでジェネレータを作成する
+def g():
+    for i in range(10):
+        yield i
+
+g = g()
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
