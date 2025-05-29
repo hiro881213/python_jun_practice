fc67ecf466a8e2c37dd3c5ad46d73d28c719ce4a
hiro881213 <nikep.godbard@gmail.com>
2025-05-30 02:14:26 +0900

    関数内でグローバル変数に値を入れる

--- chapter4_4_1_2.py
+++ chapter4_4_1_2.py
@@ -1,3 +1,12 @@
 # 4.4. 変数の有効範囲
 # 4.4.1. 変数の有効範囲
 # 関数内でグローバル変数に値を入れる
+anima = 'cat'
+
+def f():
+    global animal
+    animal = 'dog'
+    print('local:', animal)
+
+f()
+print('global:', animal)
