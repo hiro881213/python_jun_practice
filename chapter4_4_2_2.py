7d0eac2d4f77b88870a31d4629f377c9c117c948
hiro881213 <nikep.godbard@gmail.com>
2025-05-31 18:31:48 +0900

    __name__や__doc__を出力する

--- chapter4_4_2_2.py
+++ chapter4_4_2_2.py
@@ -1,3 +1,12 @@
 # 4.4. 変数の有効範囲
 # 4.4.2. ローカル変数やグローバル変数を出力する
 # __name__や__doc__を出力する
+animal = 'cat'
+
+def f():
+    """ Test func doc """
+    print(f.__name__)
+    print(f.__doc__)
+
+f()
+print('global:', __name__)
