28c82e8594a300944ed7f7ee936bc551355171ae
hiro881213 <nikep.godbard@gmail.com>
2025-06-01 16:03:14 +0900

    else

--- chapter4_5_1_3.py
+++ chapter4_5_1_3.py
@@ -1,3 +1,17 @@
 # 4.5. 例外処理(エラーハンドリング)
 # 4.5.1. 例外処理
 # 4.5.1.3. else
+l = [1, 2, 3]
+i = 5
+
+try:
+    l[0]
+
+except IndexError as ex:
+    print("Don't Worry: {}".format(ex))
+except NameError as ex:
+    print(ex)
+else:
+    print("done")
+finally:
+    print("clean up")
