300001fae44f23b106941590d58cd6ad3cd61fed
hiro881213 <nikep.godbard@gmail.com>
2025-06-01 15:57:54 +0900

    finally

--- chapter4_5_1_2.py
+++ chapter4_5_1_2.py
@@ -1,3 +1,15 @@
 # 4.5. 例外処理(エラーハンドリング)
 # 4.5.1. 例外処理
 # 4.5.1.2. finally
+l = [1, 2, 3]
+i = 5
+del l
+
+try:
+    l[i]
+except IndexError as ex:
+    print("Don't worry :{}".format(ex))
+except NameError as ex:
+    print(ex)
+finally:
+    print("clean up")
