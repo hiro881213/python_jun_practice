03e7b34ce1c1d2be15a7e5de2a29cbfe34883dae
hiro881213 <nikep.godbard@gmail.com>
2025-06-02 01:53:02 +0900

    キーの順番で並べる

--- chapter5_2_1_2.py
+++ chapter5_2_1_2.py
@@ -1,12 +1,15 @@
 # 5.2. Pythonのライブラリの使い方
 # 5.2.1. すぐに使える便利な組み込み関数
 # 5.2.1.2. sorted関数で並び替える
-# sorted関数を使わない
 ranking = {
     'A': 100,
     'B': 85,
     'C': 92
 }
 
+# sorted関数を使わない
 for key in ranking:
     print(key)
+
+# キーの順番で並べる
+print(sorted(ranking))
