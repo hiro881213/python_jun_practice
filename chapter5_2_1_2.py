03c8e5ff4c756e8fd134b3b21fd4794010ad9cf2
hiro881213 <nikep.godbard@gmail.com>
2025-06-02 01:50:06 +0900

    sorted関数を使わない

--- chapter5_2_1_2.py
+++ chapter5_2_1_2.py
@@ -1,3 +1,12 @@
 # 5.2. Pythonのライブラリの使い方
 # 5.2.1. すぐに使える便利な組み込み関数
 # 5.2.1.2. sorted関数で並び替える
+# sorted関数を使わない
+ranking = {
+    'A': 100,
+    'B': 85,
+    'C': 92
+}
+
+for key in ranking:
+    print(key)
