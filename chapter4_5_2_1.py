5109bc515d3b2378bea9905bc31c153a006380fd
hiro881213 <nikep.godbard@gmail.com>
2025-06-01 16:20:04 +0900

    move logic

--- chapter4_5_2_1.py
+++ chapter4_5_2_1.py
@@ -1,17 +1,4 @@
 # 4.5. 例外処理(エラーハンドリング)
 # 4.5.2. 独自例外
 # raise
-#raise  IndexError('test Error')
-
-# 独自の例外を作成
-class UppercaseError(Exception):
-    pass
-
-def check():
-    words = ['APPLE', 'orange', 'banana']
-
-    for word in words:
-        if word.isupper():
-            raise UppercaseError(word)
-
-check()
+raise  IndexError('test Error')
--- chapter4_5_2_2.py
+++ chapter4_5_2_2.py
@@ -1,2 +1,14 @@
 # 4.5. 例外処理(エラーハンドリング)
 # 4.5.2. 独自例外
+# 独自の例外を作成
+class UppercaseError(Exception):
+    pass
+
+def check():
+    words = ['APPLE', 'orange', 'banana']
+
+    for word in words:
+        if word.isupper():
+            raise UppercaseError(word)
+
+check()
