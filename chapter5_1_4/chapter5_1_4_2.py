bf262ee5c9f8a022433ae914002d1ebee6822b74
hiro881213 <nikep.godbard@gmail.com>
2025-06-02 01:15:38 +0900

    __init.py__とアスタリスクを使ったインポート2

--- chapter5_1_4/chapter5_1_4_2.py
+++ chapter5_1_4/chapter5_1_4_2.py
@@ -1,2 +1,8 @@
 # 5.1. 作成したパッケージをインポートする
 # 5.1.4. __init.py__とアスタリスクを使ったインポート
+from lesson_package.talk import *
+
+print(animal.sing())
+print(animal.cry())
+print(human.sing())
+print(human.cry())
