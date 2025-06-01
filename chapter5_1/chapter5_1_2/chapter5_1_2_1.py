1f22cdc0d5887bffedd3668c1ff4d53b034bdd49
hiro881213 <nikep.godbard@gmail.com>
2025-06-02 01:27:10 +0900

    add new directory

--- chapter5_1/chapter5_1_2/chapter5_1_2_1.py
+++ chapter5_1/chapter5_1_2/chapter5_1_2_1.py
@@ -2,7 +2,8 @@
 # 5.1.2. importの使い方
 # 5.1.2.1. パッケージを読むこむ
 # モジュール読み込み
+from chapter5_1.chapter5_1_2 import lesson_package
 
 # インポートした関数の呼び出し
-r = chapter5_1.chapter5_1_2.lesson_package.utils.say_twice('hello')
+r = lesson_package.utils.say_twice('hello')
 print(r)
--- chapter5_1/chapter5_1_3/chapter5_1_3_1.py
+++ chapter5_1/chapter5_1_3/chapter5_1_3_1.py
@@ -1,6 +1,6 @@
 # 5.1. 作成したパッケージをインポートする
-# 5.1.3. インんポートするときの書き方
+# 5.1.3. インポートするときの書き方
 # 5.1.3.1. asを使って違う名前で読み込む
-from chapter5_1.chapter5_1_4.lesson_package.talk import human
+from lesson_package.talk import human
 
 print(human.sing())
