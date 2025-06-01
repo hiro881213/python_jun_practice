a5ea92b85bd0c0170e861a6e18e5a245f4bd1484
hiro881213 <nikep.godbard@gmail.com>
2025-06-01 16:27:25 +0900

    既存の例外を発生させる

--- chapter4_5_2_4.py
+++ chapter4_5_2_4.py
@@ -1,2 +1,14 @@
 # 4.5. 例外処理(エラーハンドリング)
 # 4.5.2. 独自例外
+# 既存の例外を発生させる
+def check():
+    words = ['APPLE', 'orange', 'banana']
+
+    for word in words:
+        if word.isupper():
+            raise ValueError(word)
+
+try:
+    check()
+except ValueError as exc:
+    print('This is my fault. Go next')
