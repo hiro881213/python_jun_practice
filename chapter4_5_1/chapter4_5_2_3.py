9a786171c7b8f9637f9def32e07630b5ebdce584
hiro881213 <nikep.godbard@gmail.com>
2025-06-01 16:23:29 +0900

    独自の例外をexceptで実行

--- chapter4_5_1/chapter4_5_2_3.py
+++ chapter4_5_1/chapter4_5_2_3.py
@@ -1,3 +1,17 @@
 # 4.5. 例外処理(エラーハンドリング)
 # 4.5.2. 独自例外
-# 独自の例外を作成
+class UppercaseError(Exception):
+    pass
+
+def check():
+    words = ['APPLE', 'orange', 'banana']
+
+    for word in words:
+        if word.isupper():
+            raise UppercaseError(word)
+
+# 独自の例外をexceptで実行
+try:
+    check()
+except UppercaseError as ex:
+    print('This is my fault. Go next')
