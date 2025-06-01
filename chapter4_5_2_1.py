9547ada3ab6ab639539b7d567bb2edebaa68f187
hiro881213 <nikep.godbard@gmail.com>
2025-06-01 16:18:54 +0900

    独自の例外を実行

--- chapter4_5_2_1.py
+++ chapter4_5_2_1.py
@@ -6,3 +6,12 @@
 # 独自の例外を作成
 class UppercaseError(Exception):
     pass
+
+def check():
+    words = ['APPLE', 'orange', 'banana']
+
+    for word in words:
+        if word.isupper():
+            raise UppercaseError(word)
+
+check()
