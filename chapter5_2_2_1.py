6bd35ae5ea962055d77de3a6b9db9b6dabdd2247
hiro881213 <nikep.godbard@gmail.com>
2025-06-02 21:06:29 +0900

    文字列に含まれるアルファベットを数える

--- chapter5_2_2_1.py
+++ chapter5_2_2_1.py
@@ -1,2 +1,16 @@
 # 5.2. Pythonのライブラリの使い方
 # 5.2.2. Pythonの標準ライブラリ
+# 5.2.2.1. defaultdictを使用する
+
+s = "aerawjitrpqwkaerafpevpeavmovvavaf"
+
+d = {}
+
+# 文字列に含まれるアルファベットを数える
+for c in s:
+    if c not in d:
+        d[c] = 0
+
+    d[c] += 1
+
+print(d)
