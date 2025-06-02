2ab56675d853612297e60a86641c38fc8ba68a28
hiro881213 <nikep.godbard@gmail.com>
2025-06-03 01:36:03 +0900

    パッケージを読み込む場所を表示

--- chapter5_2_4/chapter5_2_4_4.py
+++ chapter5_2_4/chapter5_2_4_4.py
@@ -3,6 +3,7 @@
 # 5.2.4.2. ライブラリの場所
 # ライブラリの場所を出力する
 import collections
+import sys
 
 import termcolor
 
@@ -14,3 +15,6 @@ print(collections.__file__)
 print(termcolor.__file__)
 print(lesson_package.__file__)
 print(config.__file__)
+
+# パッケージを読み込む場所を表示
+print(sys.path)
