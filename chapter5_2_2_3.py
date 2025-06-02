0bbcf6a125771d0be67e92c1a450c1d33db3679a
hiro881213 <nikep.godbard@gmail.com>
2025-06-02 21:23:36 +0900

    collectionライブラリ

--- chapter5_2_2_3.py
+++ chapter5_2_2_3.py
@@ -1,3 +1,13 @@
 # 5.2. Pythonのライブラリの使い方
 # 5.2.2. Pythonの標準ライブラリ
 # 5.2.2.1. defaultdictを使用する
+s = "aerawjitrpqwkaerafpevpeavmovvavaf"
+
+# collectionライブラリ
+from collections import defaultdict
+d = defaultdict(int)
+
+for c in s:
+    d[c] += 1
+
+print(d)
