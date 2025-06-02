693d89d4cb8ec163831a5fc84c06fcf7b197e349
hiro881213 <nikep.godbard@gmail.com>
2025-06-03 01:33:36 +0900

    ライブラリの場所を出力する

--- chapter5_2_4/chapter5_2_4_4.py
+++ chapter5_2_4/chapter5_2_4_4.py
@@ -2,3 +2,15 @@
 # 5.2.4. ライブラリインポートの注意点
 # 5.2.4.2. ライブラリの場所
 # ライブラリの場所を出力する
+import collections
+
+import termcolor
+
+import lesson_package
+
+import config
+
+print(collections.__file__)
+print(termcolor.__file__)
+print(lesson_package.__file__)
+print(config.__file__)
