143cbedf3ab64e4e572202b272fee6aa86ab2fda
hiro881213 <nikep.godbard@gmail.com>
2025-06-04 00:49:37 +0900

    ファイルの内容を変更

--- chapter5_2_4/chapter5_2_4_3.py
+++ chapter5_2_4/chapter5_2_4_3.py
@@ -1,3 +1,15 @@
 # 5.2. Pythonのライブラリの使い方
 # 5.2.4. ライブラリインポートの注意点
 # 5.2.4.2. ライブラリの場所
+import collections
+
+import termcolor
+
+import lesson_package
+
+import config
+
+print(collections.__file__)
+print(termcolor.__file__)
+print(lesson_package.__file__)
+print(config.__file__)
--- chapter5_2_4/chapter5_2_4_4.py
+++ chapter5_2_4/chapter5_2_4_4.py
@@ -2,19 +2,7 @@
 # 5.2.4. ライブラリインポートの注意点
 # 5.2.4.2. ライブラリの場所
 # ライブラリの場所を出力する
-import collections
 import sys
-
-import termcolor
-
-import lesson_package
-
-import config
-
-print(collections.__file__)
-print(termcolor.__file__)
-print(lesson_package.__file__)
-print(config.__file__)
 
 # パッケージを読み込む場所を表示
 print(sys.path)
