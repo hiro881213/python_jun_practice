99426f5e2150f14cba92475722974262e9907bb0
hiro881213 <nikep.godbard@gmail.com>
2025-06-04 01:20:43 +0900

    add directory

--- chapter5/chapter5_1/chapter5_1_2/chapter5_1_2_1.py
+++ chapter5/chapter5_1/chapter5_1_2/chapter5_1_2_1.py
@@ -2,7 +2,7 @@
 # 5.1.2. importの使い方
 # 5.1.2.1. パッケージを読むこむ
 # モジュール読み込み
-from chapter5_1.chapter5_1_2 import lesson_package
+from chapter5.chapter5_1.chapter5_1_2 import lesson_package
 
 # インポートした関数の呼び出し
 r = lesson_package.utils.say_twice('hello')
