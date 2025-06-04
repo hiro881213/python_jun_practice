b8ea289d3fb8c091cc1f95ca727039748c82f947
hiro881213 <nikep.godbard@gmail.com>
2025-06-05 02:21:31 +0900

    自分自身のメソッドを呼び出す

--- chapter6_1_2_4.py
+++ chapter6_1_2_4.py
@@ -1,2 +1,16 @@
 # 6.1. クラスとメソッド
 # 6.1.2. クラスが初期化される時の処理
+# 自分自身のメソッドを呼び出す
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def say_something(self):
+        print('I am {}. hello'.format(self.name))
+        self.run(10)
+
+    def run(self, num):
+        print('run' * num)
+
+person = Person('Mike')
+person.say_something()
