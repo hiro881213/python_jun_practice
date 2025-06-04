8de46893b39307bef220075c3d88c61da297be3f
hiro881213 <nikep.godbard@gmail.com>
2025-06-05 02:17:20 +0900

    デフォルト引数を指定する

--- chapter6_1_2_3.py
+++ chapter6_1_2_3.py
@@ -1,2 +1,9 @@
 # 6.1. クラスとメソッド
 # 6.1.2. クラスが初期化される時の処理
+# デフォルト引数を指定する
+class Person():
+    def __init__(self, name = 'Default'):
+        self.name = name
+        print(self.name)
+
+person = Person()
