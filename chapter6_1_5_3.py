50edf8048ccffc025153137960b8cc51265e28a0
hiro881213 <nikep.godbard@gmail.com>
2025-06-07 02:02:23 +0900

    __init__のオーバーライド

--- chapter6_1_5_3.py
+++ chapter6_1_5_3.py
@@ -1,3 +1,16 @@
 # 6.1. クラスとメソッド
 # 6.1.5. 継承元のメソッドを上書きして実行する
 # 6.1.5.2. superで継承元のメソッドを呼び出す
+# initのオーバーライド
+class Car:
+    def __init__(self, model = None):
+        self.model = model
+
+class MyCar(Car):
+    pass
+
+class AdvancedCar(Car):
+    def __init__(self, model = 'SUV', enable_auto_run = False):
+        self.model = model
+        self.enable_auto_run = enable_auto_run
+
