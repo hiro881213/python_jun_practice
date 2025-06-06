0bb45ff0febdc802561f1d02cbbb57084e5b94e0
hiro881213 <nikep.godbard@gmail.com>
2025-06-07 02:18:34 +0900

    superの利用

--- chapter6_1_5_4.py
+++ chapter6_1_5_4.py
@@ -2,3 +2,14 @@
 # 6.1.5. 継承元のメソッドを上書きして実行する
 # 6.1.5.2. superで継承元のメソッドを呼び出す
 # superの利用
+class Car:
+    def __init__(self, model = None):
+        self.model = model
+
+class AdvancedCar(Car):
+    def __init__(self, model = 'SUV', enable_auto_run = False):
+        super().__init__(model)
+        self.enable_auto_run = enable_auto_run
+
+advanced_car = AdvancedCar('SUV')
+print(advanced_car.model)
