1139cf4fd8e2734cb012abde44429e0cef52285f
hiro881213 <nikep.godbard@gmail.com>
2025-06-07 01:37:49 +0900

    クラスの継承2

--- chapter6_1_4_3.py
+++ chapter6_1_4_3.py
@@ -1,2 +1,13 @@
 # 6.1. クラスとメソッド
 # 6.1.4. クラスの継承
+class Car:
+    def run(self):
+        print('run')
+
+class AdvancedCar(Car):
+    def auto_run(self):
+        print('auto run')
+
+advanced_car = AdvancedCar()
+advanced_car.run()
+advanced_car.auto_run()
