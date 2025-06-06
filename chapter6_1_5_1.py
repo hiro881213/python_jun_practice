49e2caad4c9674da71a8ecbe78ac4ceb53ad37d0
hiro881213 <nikep.godbard@gmail.com>
2025-06-07 01:47:36 +0900

    メソッドのオーバーライド

--- chapter6_1_5_1.py
+++ chapter6_1_5_1.py
@@ -1,3 +1,30 @@
 # 6.1. クラスとメソッド
 # 6.1.5. 継承元のメソッドを上書きして実行する
 # 6.1.5.1. メソッドのオーバーライド
+from chapter6_1_4.chapter6_1_4_3 import advanced_car
+
+
+class Car:
+    def run(self):
+        print('run')
+
+class MyCar(Car):
+    def run(self):
+        print('fast')
+
+class AdvancedCar(Car):
+    def run(self):
+        print('super fast')
+
+car = Car()
+car.run()
+
+print('###############')
+
+my_car = MyCar()
+my_car.run()
+
+print('###############')
+
+advanced_car = AdvancedCar()
+advanced_car.run()
