49f66ed1c5b3e8b82df9df679e0db184b5336ba8
hiro881213 <nikep.godbard@gmail.com>
2025-06-06 21:44:59 +0900

    明示的にデストラクタを呼び出す

--- chapter6_1_3_2.py
+++ chapter6_1_3_2.py
@@ -1,2 +1,12 @@
 # 6.1. クラスとメソッド
 # 6.1.3. オブジェクトの最後に実行される処理を作成する
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def __del__(self):
+        print('Good bye')
+
+person = Person('Mike')
+del person
+print('###########')
