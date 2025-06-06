8d94835b8773f0a478b243f412e4daa93bb03f0e
hiro881213 <nikep.godbard@gmail.com>
2025-06-07 01:57:27 +0900

    superで継承元のメソッドを呼び出す

--- chapter6_1_5_2.py
+++ chapter6_1_5_2.py
@@ -1,3 +1,18 @@
 # 6.1. クラスとメソッド
 # 6.1.5. 継承元のメソッドを上書きして実行する
 # 6.1.5.2. superで継承元のメソッドを呼び出す
+class Car:
+    def __init__(self, model = None):
+        self.model = model
+
+class MyCar(Car):
+    pass
+
+class AdvancedCar(Car):
+    pass
+
+my_car = MyCar('sedan')
+print(my_car.model)
+
+advanced_car = AdvancedCar('SUV')
+print(advanced_car.model)
