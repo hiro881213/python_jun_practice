68359fa1c39b7689af9f4b71b6921cc2dea440f5
hiro881213 <nikep.godbard@gmail.com>
2025-06-08 00:47:52 +0900

    BabyクラスとAdultクラスを作成する

--- chapter6_2_2_1.py
+++ chapter6_2_2_1.py
@@ -13,3 +13,16 @@ class Person:
             print('ok')
         else:
             raise Exception('No drive')
+
+class Baby(Person):
+    def __init__(self, age = 1):
+        if age < 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+
+class Adult(Person):
+    def __init__(self, age = 18):
+        if age >= 18:
+            super().__init__(age)
+        else: raise ValueError
