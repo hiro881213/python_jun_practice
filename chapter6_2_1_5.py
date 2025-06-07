ec21b396ba31c4f479875958ec91d7b4d42781c2
hiro881213 <nikep.godbard@gmail.com>
2025-06-07 15:11:26 +0900

    セッターの使い方

--- chapter6_2_1_5.py
+++ chapter6_2_1_5.py
@@ -1,3 +1,28 @@
 # 6.2. クラスの活用
 # 6.2.1. プロパティの使い方
 # 6.2.1.2. セッター
+class Car:
+    def __init__(self, model = None):
+        self.model = model
+
+class AdvancedCar(Car):
+    def __init__(self, model = 'SUV', enable_auto_run = False, passwd = '123'):
+        super().__init__(model)
+        self._enable_auto_run = enable_auto_run
+        self.passwd = passwd
+
+    @property
+    def enable_auto_run(self):
+        return self._enable_auto_run
+
+    @enable_auto_run.setter
+    def enable_auto_run(self, is_enable):
+        if self.passwd == '456':
+            self._enable_auto_run = is_enable
+        else:
+            raise ValueError
+
+advanced_car =  AdvancedCar('SUV', passwd = '456')
+advanced_car.enable_auto_run = True
+
+print(advanced_car.enable_auto_run)
