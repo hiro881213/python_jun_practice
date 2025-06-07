7c7d24affcdedad9a37d68ba85311afb08d0f67b
hiro881213 <nikep.godbard@gmail.com>
2025-06-07 15:19:18 +0900

    _をつけた変数にアクセスする

--- chapter6_2_1_6.py
+++ chapter6_2_1_6.py
@@ -1,3 +1,18 @@
 # 6.2. クラスの活用
 # 6.2.1. プロパティの使い方
 # 6.2.1.3. 変数にアンダースコアをつける
+class Car:
+    def __init__(self, model = None):
+        self.model = model
+
+class AdvancedCar(Car):
+    def __init__(self, model = 'SUV', enable_auto_run = False):
+        super().__init__(model)
+        self._enable_auto_run = enable_auto_run
+
+    @property
+    def enable_auto_run(self):
+        return self._enable_auto_run
+
+advanced_car = AdvancedCar('SUV')
+print(advanced_car.enable_auto_run)
