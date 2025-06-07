4fc3adda00e694befc75f08aa0fe9c28e73958a5
hiro881213 <nikep.godbard@gmail.com>
2025-06-07 14:37:40 +0900

    プロパティ

--- chapter6_2_1_1.py
+++ chapter6_2_1_1.py
@@ -1,3 +1,15 @@
 # 6.2. クラスの活用
 # 6.2.1. プロパティの使い方
 # 6.2.1.1. プロパティ
+class Car:
+    def __init__(self, model = 'None'):
+        self.model = model
+
+class AdvancedCar(Car):
+    def __init__(self, model = 'SUV', enable_auto_run = False):
+        super().__init__(model)
+        self.enable_auto_run = enable_auto_run
+
+advanced_car = AdvancedCar('SUV')
+advanced_car.enable_auto_run = True
+print(advanced_car.enable_auto_run)
