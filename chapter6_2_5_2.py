701524f12d7805d9e2f0653e0db08e679f110bf3
hiro881213 <nikep.godbard@gmail.com>
2025-06-08 23:58:01 +0900

    クラス変数の利用

--- chapter6_2_5_2.py
+++ chapter6_2_5_2.py
@@ -1,3 +1,17 @@
 # 6.2. クラスの活用
 # 6.2.5. クラス変数で値を共有する
 # クラス変数の利用
+class Person:
+    kind = 'human'
+
+    def __init__(self, name):
+        self.name = name
+
+    def who_are_you(self):
+        print(self.name, self.kind)
+
+a = Person('A')
+a.who_are_you()
+
+b = Person('B')
+b.who_are_you()
