026d7275b07b9094c205ff238f372627f2f50fad
hiro881213 <nikep.godbard@gmail.com>
2025-06-09 00:09:20 +0900

    リストのクラス変数

--- chapter6_2_5_3.py
+++ chapter6_2_5_3.py
@@ -1,3 +1,18 @@
 # 6.2. クラスの活用
 # 6.2.5. クラス変数で値を共有する
 # リストのクラス変数
+class T:
+    words = []
+
+    def add_word(self, word):
+        self.words.append(word)
+
+c = T()
+c.add_word('add 1')
+c.add_word('add 2')
+
+d = T()
+d.add_word('add 3')
+d.add_word('add 4')
+
+print(c.words)
