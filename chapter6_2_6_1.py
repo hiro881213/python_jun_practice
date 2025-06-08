76f24d8c89c2d22c44fd1727641f6ec6ee4a14a2
hiro881213 <nikep.godbard@gmail.com>
2025-06-09 00:27:37 +0900

    クラスメソッド

--- chapter6_2_6_1.py
+++ chapter6_2_6_1.py
@@ -1,3 +1,14 @@
 # 6.2. クラスの活用
 # 6.2.6. クラスメソッドとスタティックメソッド
 # 6.2.6.1. クラスメソッド
+class Person:
+    kind = 'human'
+
+    def __init__(self):
+        self.x = 100
+
+a = Person()
+print(a)
+
+b = Person
+print(b)
