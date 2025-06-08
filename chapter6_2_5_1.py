6d9a1c2f05eb7996887cacff06eb4a2fb5367e4b
hiro881213 <nikep.godbard@gmail.com>
2025-06-08 23:51:07 +0900

    オブジェクト間で共有するインスタンス変数

--- chapter6_2_5_1.py
+++ chapter6_2_5_1.py
@@ -1,3 +1,16 @@
 # 6.2. クラスの活用
 # 6.2.5. クラス変数で値を共有する
 # オブジェクト間で共有するインスタンス変数
+class Person:
+    def __init__(self, name):
+        self.kind = 'human'
+        self.name = name
+
+    def who_are_you(self):
+        print(self.name, self.kind)
+
+a = Person('A')
+a.who_are_you()
+
+b = Person('B')
+b.who_are_you()
