e9dd0468eb82a966526092eead772b0117dddb99
hiro881213 <nikep.godbard@gmail.com>
2025-06-09 00:18:57 +0900

    クラス変数の初期化

--- chapter6_5_4.py
+++ chapter6_5_4.py
@@ -1,3 +1,23 @@
 # 6.2. クラスの活用
 # 6.2.5. クラス変数で値を共有する
 # クラス変数の初期化
+class T:
+    words = []
+
+    def __init__(self):
+        self.words = []
+
+    def add_word(self, word):
+        self.words.append(word)
+
+c = T()
+c.add_word('add 1')
+c.add_word('add 2')
+
+print(c.words)
+
+d = T()
+d.add_word('add 3')
+d.add_word('add 4')
+
+print(d.words)
