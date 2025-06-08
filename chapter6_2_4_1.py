ff88bc9629eaf277af872e16702747aa93e79986
hiro881213 <nikep.godbard@gmail.com>
2025-06-08 23:38:19 +0900

    多重継承

--- chapter6_2_4_1.py
+++ chapter6_2_4_1.py
@@ -1,2 +1,18 @@
 # 6.2. クラスの活用
 # 6.2.4. 多重継承で複数の機能を持ったクラスを作成する
+class Person:
+    def talk(self):
+        print('talk')
+
+class Car:
+    def run(self):
+        print('run')
+
+class PersonCarRobot(Person, Car):
+    def fly(self):
+        print('fly')
+
+person_car_robot = PersonCarRobot()
+person_car_robot.talk()
+person_car_robot.run()
+person_car_robot.fly()
