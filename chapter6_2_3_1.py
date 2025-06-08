91b1b5c03a0193455acd1cc4bf39c114b9689ec2
hiro881213 <nikep.godbard@gmail.com>
2025-06-08 11:59:00 +0900

    継承クラスにそれぞれdriveメソッドを作成する

--- chapter6_2_3_1.py
+++ chapter6_2_3_1.py
@@ -1,2 +1,25 @@
 # 6.2. クラスの活用
 # 6.2.3. 抽象クラスの使い方
+class Person:
+    def __init__(self, age = 1):
+        self.age = age
+
+class Baby(Person):
+    def __init__(self, age = 1):
+        if age < 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+
+    def drive(self):
+        raise Exception('No Drive')
+
+class Adult(Person):
+    def __init__(self, age = 18):
+        if age >= 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+
+    def drive(self):
+        print('ok')
