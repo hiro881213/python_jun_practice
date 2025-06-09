65b9bfd7bdb18c171629548ea0057fbae5ad4ef1
hiro881213 <nikep.godbard@gmail.com>
2025-06-10 00:39:15 +0900

    スタティックメソッドに引数を設定する

--- chapter6_2_6_5.py
+++ chapter6_2_6_5.py
@@ -1,3 +1,9 @@
 # 6.2. クラスの活用
 # 6.2.6. クラスメソッドとスタティックメソッド
 # 6.2.6.2. スタティックメソッド
+class Person:
+    @staticmethod
+    def about(year):
+        print('about human {}'.format(year))
+
+Person.about(1999)
