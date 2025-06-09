35df35a19fff5445abba2fb279df38985ef485d3
hiro881213 <nikep.godbard@gmail.com>
2025-06-10 00:51:18 +0900

    __str__の利用

--- chapter6_2_7_1.py
+++ chapter6_2_7_1.py
@@ -1,3 +1,12 @@
 # 6.2. クラスの活用
 # 6.2.7. 特殊メソッドの利用
 # 6.2.7.1. __str__
+class Word:
+    def __init__(self, text):
+        self.text = text
+
+    def __str__(self):
+        return 'word!!!!!!!!'
+
+w = Word('test')
+print(w)
