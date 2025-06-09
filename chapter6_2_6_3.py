a93be2528fce6218ed86e11252112b100aed7377
hiro881213 <nikep.godbard@gmail.com>
2025-06-10 00:23:55 +0900

    クラスメソッドの定義

--- chapter6_2_6_3.py
+++ chapter6_2_6_3.py
@@ -1,3 +1,18 @@
 # 6.2. クラスの活用
 # 6.2.6. クラスメソッドとスタティックメソッド
 # 6.2.6.1. クラスメソッド
+class Person:
+    kind = 'human'
+
+    def __init__(self):
+        self.x = 100
+
+    @classmethod
+    def what_is_your_kind(cls):
+        return cls.kind
+
+a = Person()
+print(a.what_is_your_kind())
+
+b = Person
+print(b.what_is_your_kind())
