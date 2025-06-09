c7eaaa13502a9e1ce9582119679d79d7c0691841
hiro881213 <nikep.godbard@gmail.com>
2025-06-10 00:21:30 +0900

    メソッドの呼び出し

--- chapter6_2_6_2.py
+++ chapter6_2_6_2.py
@@ -1,3 +1,14 @@
 # 6.2. クラスの活用
 # 6.2.6. クラスメソッドとスタティックメソッド
 # 6.2.6.1. クラスメソッド
+class Person:
+    kind = 'human'
+
+    def __init__(self):
+        self.x = 100
+
+    def what_is_your_kind(self):
+        return self.kind
+
+a = Person()
+print(a.what_is_your_kind())
