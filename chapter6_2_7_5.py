dc9024d70ac14b84a4e93445902ca4c0d9ada2e1
hiro881213 <nikep.godbard@gmail.com>
2025-06-10 23:35:57 +0900

    __eq__メソッドの利用

--- chapter6_2_7_5.py
+++ chapter6_2_7_5.py
@@ -2,3 +2,14 @@
 # 6.2.7. 特殊メソッドの利用
 # 6.2.7.4. __eq__
 # __eq__メソッドの利用
+class Word:
+    def __init__(self, text):
+        self.text = text
+
+    def __eq__(self, word):
+        return self.text.lower() == word.text.lower()
+
+w = Word('test')
+w2 = Word('test')
+
+print(w == w2)
