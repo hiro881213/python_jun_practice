48ee2901e0b624aeb32fbcf729d98d469a665ff8
hiro881213 <nikep.godbard@gmail.com>
2025-06-10 23:17:25 +0900

    __len__関数

--- chapter6_2_7_2.py
+++ chapter6_2_7_2.py
@@ -1,3 +1,12 @@
 # 6.2. クラスの活用
 # 6.2.7. 特殊メソッドの利用
 # 6.2.7.2. __len__
+class Word:
+    def __init__(self, text):
+        self.text = text
+
+    def __len__(self):
+        return len(self.text)
+
+w = Word('test')
+print(len(w))
