b5aceae1722067dde760cc795af67e66fd211084
hiro881213 <nikep.godbard@gmail.com>
2025-06-10 23:28:19 +0900

    __eq__メソッドを使わない

--- chapter6_2_7_4.py
+++ chapter6_2_7_4.py
@@ -1,3 +1,14 @@
 # 6.2. クラスの活用
 # 6.2.7. 特殊メソッドの利用
 # 6.2.7.4. __eq__
+# __eq__メソッドを使わない
+class Word:
+    def __init__(self, text):
+        self.text = text
+
+w = Word('test')
+w2 = Word('test')
+
+print(w == w2)
+print(id(w))
+print(id(w2))
