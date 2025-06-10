18e776d09616a4bdd975d27d921abde2efd4b702
hiro881213 <nikep.godbard@gmail.com>
2025-06-10 23:22:13 +0900

    __add__

--- chapter6_2_7_3.py
+++ chapter6_2_7_3.py
@@ -1,3 +1,13 @@
 # 6.2. クラスの活用
 # 6.2.7. 特殊メソッドの利用
 # 6.2.7.3. __add__
+class Word:
+    def __init__(self, text):
+        self.text = text
+
+    def __add__(self, word):
+        return self.text.lower() + word.text.lower()
+
+w = Word('TEST')
+w2 = Word('#########')
+print(w + w2)
