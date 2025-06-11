263b2a5eaa02f0b2633a55fcc09744f16ded3223
hiro881213 <nikep.godbard@gmail.com>
2025-06-12 00:04:09 +0900

    ファイルを１行ずつ読み込む2

--- chapter7_1_3_4.py
+++ chapter7_1_3_4.py
@@ -1,3 +1,9 @@
 # 7.1. ファイルの基本的な操作
 # 7.1.3. ファイルの内容を読み込む
 # ファイルを１行ずつ読み込む2
+with open('test.txt', 'r') as f:
+    while True:
+        line = f.readline()
+        print(line, end='')
+
+        if not line: break
