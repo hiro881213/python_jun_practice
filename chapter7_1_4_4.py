51639f57c6f66bcc18daaade06f4eb1ba3b1e9d9
hiro881213 <nikep.godbard@gmail.com>
2025-06-14 00:33:57 +0900

    seekメソッド

--- chapter7_1_4_4.py
+++ chapter7_1_4_4.py
@@ -1,3 +1,12 @@
 # 7.1. ファイルの基本的な操作
 # 7.1.4. ファイル内の位置を移動する
 # seek
+with open('test.txt', 'r') as f:
+    f.seek(14)
+    print(f.read(1))
+
+    f.seek(15)
+    print(f.read(1))
+
+    f.seek(5)
+    print(f.read(1))
