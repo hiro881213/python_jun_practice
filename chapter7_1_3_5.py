7fe18f3833af73eff0d3ff5f19440bf105171684
hiro881213 <nikep.godbard@gmail.com>
2025-06-13 23:42:18 +0900

    ファイルをチャンクごとに読み込む

--- chapter7_1_3_5.py
+++ chapter7_1_3_5.py
@@ -1,3 +1,11 @@
 # 7.1. ファイルの基本的な操作
 # 7.1.3. ファイルの内容を読み込む
 # ファイルをチャンクごとに読み込む
+with open('test.txt', 'r') as f:
+    while True:
+        chunk = 2
+        line = f.read(chunk)
+        print(line)
+
+        if not line:
+            break
