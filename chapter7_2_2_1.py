31fa5dffd4e3b941a87a6ecedc5f7e6eebfaaa8c
hiro881213 <nikep.godbard@gmail.com>
2025-06-15 22:58:14 +0900

    データ部分を書き込む

--- chapter7_2_2_1.py
+++ chapter7_2_2_1.py
@@ -7,3 +7,5 @@ with open('test.csv', 'w') as csv_file:
     fieldnames = ['Name', 'Count']
     writer = csv.DictWriter(csv_file, fieldnames)
     writer.writeheader()
+    writer.writerow({'Name': 'A', 'Count': '1'})
+    writer.writerow({'Name': 'B', 'Count': '2'})
