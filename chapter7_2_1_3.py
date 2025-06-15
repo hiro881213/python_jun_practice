561492e5d13f5cb57b21e4d03f79361004430ba4
hiro881213 <nikep.godbard@gmail.com>
2025-06-15 22:39:28 +0900

    テキストファイルをテンプレートとして読み込む

--- chapter7_2_1_3.py
+++ chapter7_2_1_3.py
@@ -1,3 +1,9 @@
 # 7.2. ファイルの活用
 # 7.2.1. テンプレートを使って文章を作る
 # 7.2.1.2. テンプレートの活用方法
+import string
+
+with open('design/email_template.txt') as f:
+    t = string.Template(f.read())
+    contents = t.substitute(name = 'Mike', contents= 'How are you?')
+    print(contents)
