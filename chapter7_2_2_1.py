d903080076d60328b5eb291749a15cca60087866
hiro881213 <nikep.godbard@gmail.com>
2025-06-15 22:54:42 +0900

    CSVファイルの見出しを作成する

--- chapter7_2_2_1.py
+++ chapter7_2_2_1.py
@@ -1,3 +1,9 @@
 # 7.2. ファイルの活用
 # 7.2.2. CSVファイルを操作する
 # 7.2.2.1. CSVファイルへの書き込み
+import csv
+
+with open('test.csv', 'w') as csv_file:
+    fieldnames = ['Name', 'Count']
+    writer = csv.DictWriter(csv_file, fieldnames)
+    writer.writeheader()
