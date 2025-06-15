40a4113f390c3421281477425f0ea6019cf45c15
hiro881213 <nikep.godbard@gmail.com>
2025-06-15 22:33:22 +0900

    メールテンプレートを作成する

--- chapter7_2_1_2.py
+++ chapter7_2_1_2.py
@@ -1,3 +1,11 @@
 # 7.2. ファイルの活用
 # 7.2.1. テンプレートを使って文章を作る
 # 7.2.1.2. テンプレートの活用方法
+s = """\
+Hi $name.
+$contents
+Have a good day.
+"""
+
+with open('design/email_template.txt', 'w') as f:
+    f.write(s)
