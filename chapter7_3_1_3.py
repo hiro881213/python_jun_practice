94414c06648e331ec0a1af0ab43d8d4bb0a66f8c
hiro881213 <nikep.godbard@gmail.com>
2025-06-18 01:36:16 +0900

    tarファイルの中身を確認

--- chapter7_3_1_3.py
+++ chapter7_3_1_3.py
@@ -1,3 +1,8 @@
 # 7.3. 圧縮ファイルの利用
 # 7.3.1. tarファイルの圧縮と展開
 # 7.3.1.3. tarファイルを展開せずに中身を確認する
+import tarfile
+
+with tarfile.open('test.tar.gz', 'r:gz') as tr:
+    with tr.extractfile('test_dir/sub_dir/sub_test.txt') as f:
+        print(f.read())
