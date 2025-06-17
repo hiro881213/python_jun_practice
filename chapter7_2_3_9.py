75665e8c1b305af275829ca2e8fa4e11a891a264
hiro881213 <nikep.godbard@gmail.com>
2025-06-17 23:56:10 +0900

    glob.glob

--- chapter7_2_3_9.py
+++ chapter7_2_3_9.py
@@ -1,3 +1,8 @@
 # 7.2. ファイルの活用
 # 7.2.3. 様々なファイル操作
 # 7.2.3.4. ファイルやディレクトリの列挙
+import pathlib
+import glob
+
+pathlib.Path('test_dir/test_dir2/empty.txt').touch()
+print(glob.glob('test_dir/test_dir2/*'))
