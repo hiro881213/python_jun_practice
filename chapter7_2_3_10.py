3666e04a84891d94ff79246264d40ab4030e3b67
hiro881213 <nikep.godbard@gmail.com>
2025-06-18 00:00:03 +0900

    ファイルのコピー

--- chapter7_2_3_10.py
+++ chapter7_2_3_10.py
@@ -1,3 +1,8 @@
 # 7.2. ファイルの活用
 # 7.2.3. 様々なファイル操作
 # 7.2.3.5. 高度なファイル操作
+import shutil
+import glob
+
+shutil.copy('test_dir/test_dir2/empty.txt', 'test_dir/test_dir2/empty2.txt')
+print(glob.glob('test_dir/test_dir2/*'))
