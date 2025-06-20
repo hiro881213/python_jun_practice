ad0aa4a5bc3d6d0c45984398293939dd15050ad0
hiro881213 <nikep.godbard@gmail.com>
2025-06-20 23:47:28 +0900

    zipファイルの作成２

--- chapter_7_3_2_2.py
+++ chapter_7_3_2_2.py
@@ -1,4 +1,11 @@
 # 7.3. 圧縮ファイルの利用
 # 7.3.2. Zipファイルの圧縮と展開
 # 7.3.2.1. 圧縮してzipファイルを作成する
-# zipファイルの解凍
+# zipファイルの作成２
+import zipfile
+import glob
+
+with zipfile.ZipFile('test.zip', 'w') as z:
+    for f in glob.glob('test_dir/**', recursive= True):
+        print(f)
+        z.write(f)
