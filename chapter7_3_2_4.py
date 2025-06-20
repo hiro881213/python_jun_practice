cc7e2c0dc9085e7600bea1fee97ab806780c1cda
hiro881213 <nikep.godbard@gmail.com>
2025-06-20 23:58:36 +0900

    Zipファイルを展開せずに中身を確認する

--- chapter7_3_2_4.py
+++ chapter7_3_2_4.py
@@ -1,3 +1,8 @@
 # 7.3. 圧縮ファイルの利用
 # 7.3.2. Zipファイルの圧縮と展開
 # 7.3.2.3. Zipファイルを展開せずに中身を確認する
+import zipfile
+
+with zipfile.ZipFile('test.zip', 'r') as z:
+    with z.open('test_dir/test.txt') as f:
+        print(f.read())
