bccc553b9545e45eaea1d523161c0d72e2654c7a
hiro881213 <nikep.godbard@gmail.com>
2025-06-20 23:41:16 +0900

    圧縮してzipファイルを作成する

--- chapter7_3_2_1.py
+++ chapter7_3_2_1.py
@@ -1,3 +1,8 @@
 # 7.3. 圧縮ファイルの利用
 # 7.3.2. Zipファイルの圧縮と展開
 # 7.3.2.1. 圧縮してzipファイルを作成する
+import zipfile
+
+with zipfile.ZipFile('test.zip', 'w') as z:
+    z.write('test_dir')
+    z.write('test_dir/test.txt')
