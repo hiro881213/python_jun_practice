c4772d823bf06e37bf71c3f7058d310b87b61d66
hiro881213 <nikep.godbard@gmail.com>
2025-06-21 22:33:01 +0900

    処理後も残る一時ファイルを作成

--- chapter7_4_1_2.py
+++ chapter7_4_1_2.py
@@ -1,3 +1,11 @@
 # 7.4. さらに高度なファイルに関する操作
 # 7.4.1. 一時ファイルを活用する
 # 7.4.1.1. 一時ファイルの作成
+import tempfile
+
+with tempfile.NamedTemporaryFile(delete=False) as t:
+    print(t.name)
+    with open(t.name, 'w+') as f:
+        f.write('test\n')
+        f.seek(0)
+        print(f.read())
