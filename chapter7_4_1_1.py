61fed54170059ab600be36037b72c94a8d4583a0
hiro881213 <nikep.godbard@gmail.com>
2025-06-21 22:28:00 +0900

    処理終了後に消去される一時ファイルの作成

--- chapter7_4_1_1.py
+++ chapter7_4_1_1.py
@@ -1,3 +1,9 @@
 # 7.4. さらに高度なファイルに関する操作
 # 7.4.1. 一時ファイルを活用する
 # 7.4.1.1. 一時ファイルの作成
+import tempfile
+
+with tempfile.TemporaryFile(mode = 'w+') as t:
+    t.write('hello')
+    t.seek(0)
+    print(t.read())
