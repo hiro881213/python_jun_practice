e5a1e594d51e111646640d5ebe1e3acb189826fe
hiro881213 <nikep.godbard@gmail.com>
2025-06-21 22:58:57 +0900

    処理終了後も残る一時ディレクトリを作成

--- chapter7_4_1_4.py
+++ chapter7_4_1_4.py
@@ -1,3 +1,10 @@
 # 7.4. さらに高度なファイルに関する操作
 # 7.4.1. 一時ファイルを活用する
 # 7.4.1.2. 一時ディレクトリの作成
+import tempfile
+
+with tempfile.TemporaryDirectory() as td:
+    print(td)
+
+temp_dir = tempfile.mkstemp()
+print(temp_dir)
