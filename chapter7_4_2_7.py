7e7f147dbcc8ee65b111b3d7b469a1dd2629319e
hiro881213 <nikep.godbard@gmail.com>
2025-06-24 23:35:44 +0900

    popen

--- chapter7_4_2_7.py
+++ chapter7_4_2_7.py
@@ -1,3 +1,11 @@
 # 7.4. さらに高度なファイルに関する操作
 # 7.4.2. Pythonでターミナルのコマンドを実行する
 # 7.4.2.3. popenの利用
+import subprocess
+
+p1 = subprocess.Popen(['ls', '-al'], stdout = subprocess.PIPE)
+p2 = subprocess.Popen(['grep', 'test'], stdin = p1.stdout, stdout = subprocess.PIPE)
+p1.stdout.close()
+
+output = p2.communicate()[0]
+print(output)
