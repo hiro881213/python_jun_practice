fce37aaf10cdc831e08e11bc35ca697bb025a71a
hiro881213 <nikep.godbard@gmail.com>
2025-06-25 23:48:26 +0900

    年月日の表示

--- chapter7_4_3_3.py
+++ chapter7_4_3_3.py
@@ -1,3 +1,9 @@
 # 7.4. さらに高度なファイルに関する操作
 # 7.4.3. 時間にまつわるライブラリとバックアップファイル
 # 7.4.3.1. datetimeの使い方
+import datetime
+
+today = datetime.date.today()
+print(today)
+print(today.isoformat())
+print(today.strftime('%d/%m/%y'))
