2d756d5b076bd1a69335b360711dbd16b30cdcdd
hiro881213 <nikep.godbard@gmail.com>
2025-06-26 00:00:08 +0900

    時間の計算3

--- chapter7_4_3_7.py
+++ chapter7_4_3_7.py
@@ -1,3 +1,10 @@
 # 7.4. さらに高度なファイルに関する操作
 # 7.4.3. 時間にまつわるライブラリとバックアップファイル
 # 7.4.3.2. 日付・時刻の計算
+import datetime
+
+now = datetime.datetime.now()
+print(now)
+
+d = datetime.timedelta(days = 365)
+print(now - d)
