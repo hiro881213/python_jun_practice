6eb21bffefcd478bdf831fce41648725eca9f080
hiro881213 <nikep.godbard@gmail.com>
2025-06-25 23:51:44 +0900

    任意の時間の表示

--- chapter7_4_3_4.py
+++ chapter7_4_3_4.py
@@ -1,3 +1,9 @@
 # 7.4. さらに高度なファイルに関する操作
 # 7.4.3. 時間にまつわるライブラリとバックアップファイル
 # 7.4.3.1. datetimeの使い方
+import datetime
+
+t = datetime.time(hour=1, minute=10, second=5, microsecond=100)
+print(t)
+print(t.isoformat())
+print(t.strftime('%H%M%S%f'))
