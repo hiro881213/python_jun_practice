e29fd7b8b09b94d4e263cc73a77b269e0a83dd8c
hiro881213 <nikep.godbard@gmail.com>
2025-06-26 02:20:52 +0900

    バックアップファイルの作成

--- chapter7_4_3_10.py
+++ chapter7_4_3_10.py
@@ -1,3 +1,16 @@
 # 7.4. さらに高度なファイルに関する操作
 # 7.4.3. 時間にまつわるライブラリとバックアップファイル
 # 7.4.3.4. バックアップファイルの作成
+import os
+import shutil
+import datetime
+
+now =  datetime.datetime.now()
+
+file_name = 'test.txt'
+
+if os.path.exists(file_name):
+    shutil.copy(file_name, "{}.{}".format(file_name, now.strftime('%Y_%m_%d_%H_%M_%S')))
+
+with open(file_name, 'w') as f:
+    f.write('test')
